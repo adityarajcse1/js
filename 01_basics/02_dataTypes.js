3eacc1ca60240e65813ae858a78f8c466d3f8762
Aditya Raj <adityaraj162536@gmail.com>
2026-06-24 13:17:21 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let name = ("Aditya")
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof age);
