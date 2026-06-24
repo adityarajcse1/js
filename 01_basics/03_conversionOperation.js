3d83f6afcba3e3aa5d89a33cf5ae309160e840ba
Aditya Raj <adityaraj162536@gmail.com>
2026-06-24 13:28:33 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33"
+
+console.log(typeof score)           //string
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);  //number
+
+/* "33" => 33
+    "33abc" => NaN
+    true
+    true => 1;  false => 0  */
