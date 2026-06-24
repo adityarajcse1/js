19bf5a941d9f0e82ee26c0b2032fdc973e1f39ac
Aditya Raj <adityaraj162536@gmail.com>
2026-06-24 13:56:41 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,3 +8,35 @@ console.log(typeof valueInNumber);  //number
     "33abc" => NaN
     true
     true => 1;  false => 0  */
+
+    //*********  operations *********//
+    let value = 3 
+    let netValue = -value //-ve value
+
+    console.log (2 + 2)
+    console.log (2 - 2)
+    console.log (2 * 2)
+    console.log (2 / 2)
+    console.log (2 % 2);
+
+    let str1  = "Aditya "
+    let str2 = "Raj"
+
+    let str3 = str1 + str2
+
+    console.log(str3);
+
+    console.log("1" + 2) //12
+    console.log(1 + "2" ) //12
+    console.log("1" + 2 + 2) //122
+    console.log(1 + 2 + "2"); //32
+
+    console.log(true) //true
+    console.log(+true) //1
+    console.log(+""); //0
+
+
+    let count = 10
+    count++ // 11
+    ++count //11
+    console.log(count)
