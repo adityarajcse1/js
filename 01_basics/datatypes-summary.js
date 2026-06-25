1f920282b1a477bb8391ed4ae59e03b2a3896ad8
Aditya Raj <adityaraj162536@gmail.com>
2026-06-25 16:05:13 +0000

    dataTypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+
+// Reference type (non-primitive)
+
+//Array, Objects, Functions
+
+ const heros = ["Saktiman", "Spiderman", "Ironman"]
+ let myObj = {
+    name : "Aditya",
+    age: 22
+ }
+
+ const myFunction = function(){
+    console.log("Hello world");
+ }
